cea987179246df6fb981268fd5985aa36f4eeeb6
7anu7 <anushababy7777@gmail.com>
2018-09-28 14:27:01 +0530

    Add files via upload
    ss

---
+++ inp.py
@@ -0,0 +1,11 @@
+#using input function
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+sum=a+b
+sub=a-b
+div=a/b
+mult=a*b
+print(sum)
+print(sub)
+print(div)
+print(mult)
---
+++ int.py
@@ -0,0 +1,8 @@
+#finding the greater number
+x=int(input("enter the value of x"))
+y=int(input("enter the value of y"))
+if(x>y):
+	print("x is the greatest number")
+else:
+	print("y is the greatest number")
+	
